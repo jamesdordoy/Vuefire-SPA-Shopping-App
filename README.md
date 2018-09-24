80ecfc4827338f3e9c3702e547c84ece763c9bc3
Home <jamesdordoy@gmail.com>
2018-09-24 12:16:49 +0100

    Set up Tailwind.css

--- README.md
+++ README.md
@@ -5,6 +5,11 @@
 npm install
 ```
 
+### Compiles tailwind assets
+```
+npm run tailwind
+```
+
 ### Compiles and hot-reloads for development
 ```
 npm run serve
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,6 +27,22 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+  <img class="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains">
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p class="text-grey-darker text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div class="px-6 py-4">
+    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#photography</span>
+    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#travel</span>
+    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#winter</span>
+  </div>
+</div>
+
   </div>
 </template>
 
